f3fd28a812324cc720a65279607dc0dd5f4f0ca3
pang <pangton28.p@gmail.com>
2023-12-25 18:15:17 +0700

    feat(exercise) : add complete problem-solving

--- ex-1.js
+++ ex-1.js
@@ -2,8 +2,35 @@
 
 const romanToInt = function (s) {
   //Start coding here
+  const romanNumbers = {
+    'I': 1,
+    'V': 5,
+    'X': 10,
+    "L": 50,
+    "C": 100,
+    "D": 500,
+    "M": 1000
+  }
+let result = 0;
+
+for (let i = 0; i < s.length; i++) {
+  const currentNumber = romanNumbers[s[i]];
+  const nextNumber = romanNumbers[s[i+1]];
+  
+  if (nextNumber > currentNumber) {
+    result = result + nextNumber - currentNumber;
+    i++;
+  } else {
+    result = result + currentNumber
+  }
+}
+  return result;
 };
 
 const result1 = romanToInt("III"); // 3
 const result2 = romanToInt("LVIII"); // 58
 const result3 = romanToInt("MCMXCIV"); //1994
+
+console.log(result1);
+console.log(result2);
+console.log(result3);
--- ex-2.js
+++ ex-2.js
@@ -2,6 +2,21 @@
 
 const fizzBuzz = function (n) {
   //Start coding here
+  const result = [];
+
+  for (let i = 1; i <= n; i++) {
+    let current = "";
+
+    if (i % 3 === 0) {
+      current = current + "Fizz";
+    } else if (i % 5 === 0) {
+      current = current + "Buzz";
+    } else if (current === "") {
+      current = i.toString();
+    }
+    result.push(current);
+  }
+  return result;
 };
 
 const result1 = fizzBuzz(3);
